3b44090a18606b7627e51a333534fba4d836241f
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2011-05-01 23:54:08 +0100

    db stuff

--- phpappfolder/docs/initialsetup.sql
+++ phpappfolder/docs/initialsetup.sql
@@ -1,4 +1,16 @@
 CREATE DATABASE  `WebBasedControlSystem` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER 'wbcs'@'localhost' IDENTIFIED BY  'wbcs1234';
+GRANT SELECT , INSERT , UPDATE , DELETE ON  `WebBasedControlSystem` . * TO  'wbcs'@'localhost';
+
+
+CREATE TABLE `WebBasedControlSystem`.`soapy` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ident` varchar(32) NOT NULL,
+  `soapmsg` varchar(512) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ident` (`ident`)
+) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
+
 
 CREATE TABLE  `WebBasedControlSystem`.`user` (
 `id` INT NOT NULL AUTO_INCREMENT ,
@@ -8,8 +20,5 @@ CREATE TABLE  `WebBasedControlSystem`.`user` (
 `password` VARCHAR( 32 ) NOT NULL ,
 `phonenumber` VARCHAR( 16 ) NOT NULL ,
 PRIMARY KEY (  `id` )
-) ENGINE = MYISAM CHARACTER SET utf8 COLLATE utf8_general_ci;
+) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-CREATE USER 'wbcs'@'localhost' IDENTIFIED BY  'wbcs1234';
-
-GRANT SELECT , INSERT , UPDATE , DELETE ON  `WebBasedControlSystem` . * TO  'wbcs'@'localhost';
