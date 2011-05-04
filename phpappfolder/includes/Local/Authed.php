f7d97bfd45e16e7dc022cec98b61a74426bd70cb
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2011-05-04 01:28:57 +0100

    Fixed redirect on log out

--- phpappfolder/includes/Local/Authed.php
+++ phpappfolder/includes/Local/Authed.php
@@ -8,12 +8,13 @@ class Local_Authed {
 
 	public function isAuthed() {
 		Models_SessionModel::checkAndRedirect();
-		if ( time() >= $_SESSION['last_access'] + (10*60) ) {
+		if ( array_key_exists("last_access", $_SESSION) && time() >= $_SESSION['last_access'] + (10*60) ) {
 			Models_SessionModel::destroysession ( );
 			header("Location: /p07224405");
 		}
-		else
+		else if ( array_key_exists("last_access", $_SESSION) ) {
 			$_SESSION['last_access'] = time();
+		}
 	}
 
 }
